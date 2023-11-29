8372cb39aacdf17f1547a10fc0656e83ec29b4ff
qinyujie <943868899@qq.com>
2023-11-30 00:16:11 +0800

    文档中心链接

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -44,6 +44,20 @@ export default {
                     {
                         text: "学习交流",
                         link: "/0.1.md",
+                    },
+                    {
+                        text: "知识答疑",
+                        link: "/0.2.md",
+                    }
+                ],
+            },
+            {
+                text: "附录",
+                collapsible: true,
+                items: [
+                    {
+                        text: "随书示例代码",
+                        link: "/samples.md",
                     }
                 ],
             },
@@ -60,8 +74,80 @@ export default {
                         link: "/2.md",
                     },
                     {
-                        text: "随书示例代码",
-                        link: "/samples.md",
+                        text: "第3章 实体和值对象",
+                        link: "/3.md",
+                    },
+                    {
+                        text: "第4章 聚合与聚合根",
+                        link: "/4.md",
+                    },
+                    {
+                        text: "第5章 工厂、仓储和领域服务",
+                        link: "/5.md",
+                    },
+                    {
+                        text: "第6章 设计模式",
+                        link: "/6.md",
+                    },
+                    {
+                        text: "第7章 防腐层",
+                        link: "/7.md",
+                    },
+                    {
+                        text: "第8章 领域事件",
+                        link: "/8.md",
+                    },
+                    {
+                        text: "第9章 CQRS",
+                        link: "/9.md",
+                    },
+                    {
+                        text: "第10章 事件溯源",
+                        link: "/10.md",
+                    },
+                    {
+                        text: "第11章 一致性",
+                        link: "/11.md",
+                    },
+                    {
+                        text: "第12章 战略设计",
+                        link: "/12.md",
+                    },
+                    {
+                        text: "第13章 领域建模",
+                        link: "/13.md",
+                    },
+                    {
+                        text: "第14章 研发效能",
+                        link: "/14.md",
+                    },
+                    {
+                        text: "第15章 测试驱动开发",
+                        link: "/15.md",
+                    },
+                    {
+                        text: "第16章 敏捷开发",
+                        link: "/16.md",
+                    },
+                    {
+                        text: "第17章 C4架构模型",
+                        link: "/17.md",
+                    },
+                    {
+                        text: "第18章 使用DDD进行系统重构",
+                        link: "/18.md",
+                    },
+                    {
+                        text: "第19章 团队领域驱动设计",
+                        link: "/19.md",
+                    },
+                    {
+                        text: "第20章 使用DDD开发直播服务",
+                        link: "/20.md",
+                    },
+                    {
+                        text: "第21章 使用DDD开发AIGC产品",
+                        link: "/21.md",
                     }
                 ],
             }
